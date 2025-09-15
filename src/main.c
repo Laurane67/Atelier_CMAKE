cbb3167c21866c7b46be6f1ebaffdca4e1e6dccd
Laurane Deckers <laurane.deckers@epita.fr>
2025-09-15 14:41:15 +0200

    add car function

--- src/main.c
+++ src/main.c
@@ -27,8 +27,19 @@ int main(int argc, char *argv[]){
 		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
+	return 0;
+	}
+	if (argc == 3)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	double r = 0;
+	if (strcmp(op, "car") == 0) {
+		r = _square(atof(a));
+		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
+	}
 	return 0;
 }
 
